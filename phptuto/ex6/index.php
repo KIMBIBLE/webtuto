88eb5b755911e7590168397893a4df351b594318
bbkim <jotun9935@gmail.com>
2018-04-29 09:47:31 +0000

    function app

--- phptuto/ex6/index.php
+++ phptuto/ex6/index.php
@@ -1,36 +1,54 @@
+<?php
+function print_title(){
+    if(isset($_GET['id']))
+        echo $_GET['id'];
+    else
+        echo "Welcome";
+}
+
+function print_list(){
+    $base_dir = "./data";
+    $list = scandir("$base_dir");
+    //var_dump($list);
+
+    $cnt=count($list);
+    for($i=2; $i<$cnt; $i++){
+        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+    }
+}
+
+function print_description(){
+    if(isset($_GET['id']))
+        echo file_get_contents("data/".$_GET['id']);
+    else
+        echo "Hello, PHP";
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>WEB-PHP</title>
+        <title>
+            <?php
+            print_title();
+            ?>
+        </title>
     </head>
     <body>
         <h1><a href="index.php">WEB</a></h1>
         <ol>
             <?php
-                $base_dir = "./data";
-                $list = scandir("$base_dir");
-                //var_dump($list);
-
-                $cnt=count($list);
-                for($i=2; $i<$cnt; $i++){
-                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-                }
+            print_list();
             ?>
         </ol>
         <h2>
             <?php
-                if(isset($_GET['id']))
-                    echo $_GET['id'];
-                else
-                    echo "Welcome";
+            print_title();
             ?>
         </h2>
         <?php
-            if(isset($_GET['id']))
-                echo file_get_contents("data/".$_GET['id']);
-            else
-                echo "Hello, PHP";
+            print_description()
         ?>
 
     </body>
