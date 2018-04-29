b23d30cfe6142c4532054f7e117ea0d597ca0fd0
bbkim <jotun9935@gmail.com>
2018-04-29 10:58:16 +0000

    CRUD:create

--- phptuto/ex6/create.php
+++ phptuto/ex6/create.php
@@ -43,14 +43,16 @@ function print_description(){
             ?>
         </ol>
         <p><a href="create.php">create</a></p>
-        <h2>
-            <?php
-            print_title();
-            ?>
-        </h2>
-        <?php
-            print_description()
-        ?>
-
+        <form action="create_process.php" method="post">
+            <p>
+                <input type="text" name="title" placeholder="INPUT TITLE HERE">
+            </p>
+            <p>
+                <textarea name="description" placeholder="INPUT DESCRIPTION HERE"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
     </body>
 </html>
